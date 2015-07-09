969060686c68869fd86743aa447f25241d98ecbb
Douglas Wu <wckdouglas@gmail.com>
2015-07-09 15:27:41 -0500

    Merge branch 'master' of github.com:wckdouglas/filterSamFile

--- src/tRNAparsePileupNew.cpp
+++ src/tRNAparsePileupNew.cpp
@@ -33,7 +33,7 @@ int usage(char *argv[])
 int printingTable(string transcriptID, string mispos, string ref, 
 				int cov, string  modifiedBase, 
 				int A, int C, int T, int G, 
-				int insertion, int deletion, int refCount, int end)
+				int insertion, int deletion, int refCount, int block)
 {
     int i;
     // correct format for read data in R
@@ -52,12 +52,12 @@ int printingTable(string transcriptID, string mispos, string ref,
 
     // print lines
     cout << transcriptID << "\t" << mispos << "\t" << ref << "\t";
-    cout << cov << "\t"; 
+    cout << cov + block<< "\t"; 
 	cout << modifiedBase << "\t";
 	cout << A << "\t" << C << "\t";
 	cout << T << "\t" << G  << "\t" ;
 	cout << insertion << "\t" << deletion;
-	cout << "\t" << end;
+	cout << "\t" << block;
 	cout <<'\n';
     return 0;
 }
@@ -65,7 +65,7 @@ int printingTable(string transcriptID, string mispos, string ref,
 // processing lines with mismatches 
 int extractMismatches(string reads, string baseQuals, int cov, 
                     string transcriptID, string mispos, 
-					string ref, string modifiedBase, int qualThreshold, int coverageThreshold)
+					string ref, string modifiedBase, int qualThreshold, int coverageThreshold, int &prePos, int &preEnd)
 {
     int start = 0, end = 0, i = 0;
 	int A = 0, C = 0, T = 0, G = 0, N = 0; 
@@ -73,26 +73,38 @@ int extractMismatches(string reads, string baseQuals, int cov,
 	int qual;
 	int insertion = 0, deletion = 0, current = 0;
 	int refCount = 0;
+	int block;
+	if (prePos == atoi(mispos.c_str()) - 1 )
+	{
+		block = preEnd;
+	}
+	else
+	{
+		block = 0;
+	}
 	fixpileup(A, C, T, G, N,
 			a, c, t, g, n,
 			deletion, insertion, reads, baseQuals,
 			qualThreshold, cov, refCount, start, end);
 	cov += deletion;
-	if (cov > coverageThreshold && refCount != cov)
+	if (modifiedBase != "A" && modifiedBase != "C" && modifiedBase != "G" 
+		&& modifiedBase != "T" && cov > coverageThreshold && refCount != cov)
 	{
-		printingTable(transcriptID, mispos, ref, cov, modifiedBase, 
-					A, C, T, G, insertion, deletion, refCount,end);
 		assert (N + A + T + G + C + 
 				a + c + t + g + n + 
 				refCount + deletion == cov);
+		printingTable(transcriptID, mispos, ref, cov, modifiedBase, 
+					A, C, T, G, insertion, deletion, refCount,block);
 	}	
+	preEnd = end;
+	prePos = atoi(mispos.c_str());
     return 0;
 }
 
 
 // extract from each line different columns
 // and give them to further processing
-int processLine( stringList columns, seq_map &seqIndex, int qualThreshold, int coverageThreshold) 
+int processLine( stringList columns, seq_map &seqIndex, int qualThreshold, int coverageThreshold, int &prePos, int &preEnd) 
 {
     if (columns[2] != "N" && columns[2] != "." && columns[2] != "_")
     {
@@ -101,21 +113,15 @@ int processLine( stringList columns, seq_map &seqIndex, int qualThreshold, int c
         if (columns.size() == 6) 
         {
             cov = atoi(columns[3].c_str());
-            if (cov > coverageThreshold)
-            { 
-                transcriptID = columns[0];
-                pos = columns[1];
-                ref = columns[2];
-                reads = columns[4];
-                baseQuals = columns[5];
-                modifiedBase = seqIndex[transcriptID][atoi(pos.c_str()) - 1]  ;
-                assert ( baseQuals.length() == cov ) ;
-				if (modifiedBase != "A" && modifiedBase != "C" && modifiedBase != "G" && modifiedBase != "T")
-				{
-					extractMismatches(reads, baseQuals, cov, transcriptID, 
-									pos, ref, modifiedBase, qualThreshold, coverageThreshold);
-				}
-            }
+			transcriptID = columns[0];
+			pos = columns[1];
+			ref = columns[2];
+			reads = columns[4];
+            baseQuals = columns[5];
+            modifiedBase = seqIndex[transcriptID][atoi(pos.c_str()) - 1]  ;
+            assert ( baseQuals.length() == cov ) ;
+			extractMismatches(reads, baseQuals, cov, transcriptID, 
+							pos, ref, modifiedBase, qualThreshold, coverageThreshold, prePos, preEnd);
         }
     }
     return 0;
@@ -128,10 +134,12 @@ int processLine( stringList columns, seq_map &seqIndex, int qualThreshold, int c
 int readFile(const char* filename, seq_map &seqIndex, int qualThreshold, int coverageThreshold)
 {
     ifstream myfile(filename);
+	int prePos;
+	int preEnd;
     for (string line; getline(myfile, line);)
     {
         stringList columns = split(line,'\t');
-        processLine(columns, seqIndex, qualThreshold, coverageThreshold);
+        processLine(columns, seqIndex, qualThreshold, coverageThreshold,prePos,preEnd);
     }
     return 0;
 }
@@ -141,10 +149,12 @@ int readFile(const char* filename, seq_map &seqIndex, int qualThreshold, int cov
 // parse it line by line
 int readStream(seq_map &seqIndex, int qualThreshold, int coverageThreshold)
 {
+	int prePos;
+	int preEnd;
     for (string line; getline(cin, line);)
     {
         stringList columns = split(line,'\t');
-        processLine(columns, seqIndex, qualThreshold, coverageThreshold);
+        processLine(columns, seqIndex, qualThreshold, coverageThreshold, prePos, preEnd);
     }
     return 0;
 }
